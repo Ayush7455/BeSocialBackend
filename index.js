878a550ec81711b6ee60b9095fcb5f067fbe337d
Ayush757 <ayushhh.a757@gmail.com>
2023-01-30 18:22:09 +0530

    Port Changed and Socket IO bug fix

--- index.js
+++ index.js
@@ -6,16 +6,46 @@ require('./db');
 require('./models/User');
 const authRoutes = require('./routes/authRoutes');
 const uploadMediaRoutes = require('./routes/uploadMediaRoutes');
+const messageRoutes = require('./routes/messageRoutes');
+const { createServer } = require("http");
+const { Server } = require("socket.io");
+
+const httpServer = createServer();
+
+
+const io = new Server(httpServer, { /* options */ });
+
 
 app.use(bodyParser.json());
 app.use(authRoutes);
 app.use(uploadMediaRoutes);
+app.use(messageRoutes);
 
 app.get('/', (req, res) => {
     res.send("Hello World");
 })
 
+io.on("connection", (socket) => {
 
+    console.log("USER CONNECTED - ", socket.id);
+
+    socket.on("disconnect", () => {
+        console.log("USER DISCONNECTED - ", socket.id);
+    });
+
+    socket.on("join_room", (data) => {
+        console.log("USER WITH ID - ",socket.id,"JOIN ROOM - ", data.roomid);
+        socket.join(data);
+    });
+
+    socket.on("send_message", (data) => {
+        console.log("MESSAGE RECEIVED - ", data);
+        io.emit("receive_message", data);
+    });
+});
+
+
+httpServer.listen(port);
 app.listen(port, () => {
     console.log("Server is running on port " + port);
 })
