dd7fa6b2629c51f878e6c3ff3a8fb46639d5f5c5
Ayush757 <ayushhh.a757@gmail.com>
2023-01-30 19:03:51 +0530

    Port Changed and Socket IO bug fix

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const port = process.env.PORT;
+const port = 3000;
 const app = express();
 const bodyParser = require('body-parser');
 require('./db');
@@ -56,7 +56,7 @@ io.on("connection", (socket) => {
 });
 
 
-httpServer.listen(3000);
+httpServer.listen(3001);
 
 // ...
 app.listen(port, () => {
